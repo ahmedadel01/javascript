a088916e7f6d9aae97e25ac126d5e0583f62a175
ahmed adel <71907551+ahmedadel01@users.noreply.github.com>
2023-10-02 17:45:09 +0300

    Create three_fizz_five_bizz.js

---
+++ three_fizz_five_bizz.js
@@ -0,0 +1,22 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  
+  if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBizz");
+  }
+  else if (count % 3 === 0) {
+      output.push("Fizz");
+  }
+  else if (count % 5 === 0) {
+      output.push("Bizz");
+    }
+  else {
+      output.push(count);
+    }
+  
+  count++;
+  
+  console.log(output);
+}
